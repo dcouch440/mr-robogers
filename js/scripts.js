20c12f9fde0442c7c1178bdc3fca8d46653d3b61
dcouch440 <dcouch440@gmail.com>
2021-01-22 10:34:29 -0800

    remove unnecessary code, all test still pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 const robogersTalk = (arg) => {
   const returnArray = [];
   for (let i = 0; i <= arg; i++) {
-    const iterateSplit = arg > 9 ? `${i}`.split('') : `${i}`.split('')
+    const iterateSplit = `${i}`.split('')
     if (iterateSplit.includes('3')) {
       returnArray.push('Won\'t you be my neighbor?');
     }else if (iterateSplit.includes('2')) {
@@ -15,4 +15,4 @@ const robogersTalk = (arg) => {
   }
   return returnArray;
 };
-console.log(robogersTalk(13))
+console.log(robogersTalk(300))
